f1f02375268c0e517b69489b4fb337d24649e5c9
RamaRamdani03 <ramaramdani449@gamil.com>
2024-10-20 23:18:38 +0700

    Menambahkan 2 objek pada file hp

--- routes/hp.js
+++ routes/hp.js
@@ -1,12 +1,24 @@
-import express, {Router} from "express";
+import express, { Router } from "express";
 const router = express.Router();
 const hp = [
-{
-    Nama: "Rama Ramdani",
-    Merk: "Samsung",
-    tipe: "A 15",
-    warna: "Blue",
-},
+    {
+        Nama: "Rama Ramdani",
+        Merk: "Samsung",
+        tipe: "A 15",
+        warna: "Blue",
+    },
+    {
+        Nama: "Dynan",
+        Merk: "Iphone",
+        tipe: "Ip 13",
+        warna: "Pink",
+    },
+    {
+        Nama: "capung",
+        Merk: "Google Pixel",
+        tipe: "8 pro",
+        warna: "white",
+    }
 ];
 router.get("/", (req, res) => {
     res.send(hp);
